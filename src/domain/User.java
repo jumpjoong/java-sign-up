7c9f3f4783d180a917a917b4de42406c90bb627c
kimhyunjoong <100519998+khj0196@users.noreply.github.com>
2025-04-24 16:35:59 +0900

    first commit

---
+++ src/domain/User.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class User {
+    private String id;
+    private String pw;
+
+    public User(String id, String pw) {
+        this.id = id;
+        this.pw = pw;
+    }
+    //아이디 중복 여부
+    public String getUserId () {
+        return id;
+    }
+    //로그인할 때 사용할 메서드
+    public boolean checkPw (String pw) {
+        return this.pw.equals(pw);
+    }
+}
